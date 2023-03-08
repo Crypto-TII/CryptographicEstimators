e95aa95777eafc02370598d8df3add3ff14994cb
ariane <arianezf@gmail.com>
2023-03-08 16:10:19 +0000

    fix: update copyright text

--- scripts/create_copyright.py
+++ scripts/create_copyright.py
@@ -1,8 +1,21 @@
 import os
 
-COPYRIGHT = """
-# Copyright 2023 
-"""
+COPYRIGHT = "# ****************************************************************************\n" + \
+"# \t\tCopyright 2023 Technology Innovation Institute\n" + \
+"# \n" + \
+"# \tThis program is free software: you can redistribute it and/or modify\n" + \
+"# \tit under the terms of the GNU General Public License as published by\n" + \
+"# \tthe Free Software Foundation, either version 3 of the License, or\n" + \
+"# \t(at your option) any later version.\n" + \
+"# \n" + \
+"# \tThis program is distributed in the hope that it will be useful,\n" + \
+"# \tbut WITHOUT ANY WARRANTY; without even the implied warranty of\n" + \
+"# \tMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n" + \
+"# \tGNU General Public License for more details.\n" + \
+"# \n" + \
+"# \tYou should have received a copy of the GNU General Public License\n" + \
+"# \talong with this program.  If not, see <https://www.gnu.org/licenses/>.\n" + \
+"# ****************************************************************************\n "
 
 ROOT_FOLDER = "cryptographic_estimators"
 FILES_TO_READ = os.walk(ROOT_FOLDER)
