c27940027ebb8737a58c5dfe1ba36ed23b82f23b
AnonymousAccABC <andre.esser@tii.ae>
2023-03-09 09:42:57 +0400

    feature: add PEProblem class

---
+++ cryptographic_estimators/PEEstimator/pe_constants.py
@@ -0,0 +1,3 @@
+PE_CODE_LENGTH = "code length"
+PE_CODE_DIMENSION = "code dimension"
+PE_FIELD_SIZE = "field size"
--- cryptographic_estimators/PEEstimator/pe_problem.py
+++ cryptographic_estimators/PEEstimator/pe_problem.py
@@ -1,5 +1,6 @@
 from ..base_problem import BaseProblem
-
+from .pe_constants import *
+from math import log2, factorial
 
 class PEProblem(BaseProblem):
     """
@@ -12,44 +13,56 @@ class PEProblem(BaseProblem):
     - ``nsolutions`` -- number of (expected) solutions of the problem in logarithmic scale
     """
 
-    def __init__(self, **kwargs):  # Fill with parameters
+    def __init__(self, n: int, k: int, q: int, **kwargs):
         super().__init__(**kwargs)
+        self.parameters[PE_CODE_LENGTH] = n
+        self.parameters[PE_CODE_DIMENSION] = k
+        self.parameters[PE_FIELD_SIZE] = q
+        self.nsolutions = kwargs.get("nsolutions", max(self.expected_number_solutions(), 0))
 
     def to_bitcomplexity_time(self, basic_operations: float):
         """
-        Returns the bit-complexity corresponding to basic_operations field additions
+        Returns the bit-complexity corresponding to basic_operations Fq additions
 
         INPUT:
 
         - ``basic_operations`` -- Number of field additions (logarithmic)
 
         """
-        pass
+        _, _, q = self.get_parameters()
+        return basic_operations + log2(q)
 
-    def to_bitcomplexity_memory(self, basic_operations: float):
+    def to_bitcomplexity_memory(self, elements_to_store: float):
         """
-        Returns the memory bit-complexity associated to a given number of elements to store
+        Returns the memory bit-complexity associated to a given number of Fq elements to store
 
         INPUT:
 
-        - ``elements_to_store`` -- number of memory operations (logarithmic)
+        - ``elements_to_store`` -- number of elements to store (logarithmic)
 
         """
-        pass
+        _, _, q = self.get_parameters()
+        elements_to_store + log2(q)
 
     def expected_number_solutions(self):
         """
         Returns the logarithm of the expected number of existing solutions to the problem
 
         """
-        pass
+        n,k,q=self.get_parameters()
+        return log2(q)*k*k+log2(factorial(n))-log2(q)*n*k
 
     def __repr__(self):
         """
         """
-        pass
+        n, k, q = self.get_parameters()
+        rep = "permutation equivalence problem with (n,k,q) = " \
+              + "(" + str(n) + "," + str(k) + "," + str(q) + ")"
+
+        return rep
 
     def get_parameters(self):
         """
+        Returns n, k, q
         """
-        pass
+        return self.parameters.values()
