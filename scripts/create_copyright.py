ea2076d24af283262518434c58da5b888e2eae65
ariane <arianezf@gmail.com>
2023-03-09 08:54:38 +0000

    fix: delete tab from copyright template

--- scripts/create_copyright.py
+++ scripts/create_copyright.py
@@ -1,20 +1,20 @@
 import os
 
 COPYRIGHT = "# ****************************************************************************\n" + \
-"# \t\tCopyright 2023 Technology Innovation Institute\n" + \
+"# Copyright 2023 Technology Innovation Institute\n" + \
 "# \n" + \
-"# \tThis program is free software: you can redistribute it and/or modify\n" + \
-"# \tit under the terms of the GNU General Public License as published by\n" + \
-"# \tthe Free Software Foundation, either version 3 of the License, or\n" + \
-"# \t(at your option) any later version.\n" + \
+"# This program is free software: you can redistribute it and/or modify\n" + \
+"# it under the terms of the GNU General Public License as published by\n" + \
+"# the Free Software Foundation, either version 3 of the License, or\n" + \
+"# (at your option) any later version.\n" + \
 "# \n" + \
-"# \tThis program is distributed in the hope that it will be useful,\n" + \
-"# \tbut WITHOUT ANY WARRANTY; without even the implied warranty of\n" + \
-"# \tMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n" + \
-"# \tGNU General Public License for more details.\n" + \
+"# This program is distributed in the hope that it will be useful,\n" + \
+"# but WITHOUT ANY WARRANTY; without even the implied warranty of\n" + \
+"# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n" + \
+"# GNU General Public License for more details.\n" + \
 "# \n" + \
-"# \tYou should have received a copy of the GNU General Public License\n" + \
-"# \talong with this program.  If not, see <https://www.gnu.org/licenses/>.\n" + \
+"# You should have received a copy of the GNU General Public License\n" + \
+"# along with this program.  If not, see <https://www.gnu.org/licenses/>.\n" + \
 "# ****************************************************************************\n "
 
 ROOT_FOLDER = "cryptographic_estimators"
