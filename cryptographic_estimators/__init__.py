3d9633db7806392f0b153ce86ac355c84a8a6af8
Floyd Zweydinger <floyd.zweydinger@rub.de>
2023-03-13 11:14:17 +0100

    forgot to add the SDFq Estimator to the root __init__ file

--- cryptographic_estimators/__init__.py
+++ cryptographic_estimators/__init__.py
@@ -3,5 +3,6 @@ from .base_estimator import BaseEstimator
 from .base_problem import BaseProblem
 from .helper import ComplexityType, memory_access_cost, concat_pretty_tables, _truncate, round_or_truncate
 from . import SDEstimator
+from . import SDFqEstimator
 from . import MQEstimator
 from . import DummyEstimator
