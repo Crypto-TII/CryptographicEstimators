6b99b6c963a1b1774a4bba98eb7de8733fc4cc55
franjpr <franpr@leanmind.es>
2023-03-20 08:49:13 +0000

    feat: add raised TypeError example

--- tests/test_estimators.py
+++ tests/test_estimators.py
@@ -1,4 +1,4 @@
-from cryptographic_estimators.MQEstimator import MQEstimator, Bjorklund, BooleanSolveFXL, F5, HybridF5, KPG, CGMTA, MHT, Lokshtanov, DinurFirst, DinurSecond, Crossbred
+from cryptographic_estimators.MQEstimator import MQEstimator, MQProblem, Bjorklund, BooleanSolveFXL, F5, HybridF5, KPG, CGMTA, MHT, Lokshtanov, DinurFirst, DinurSecond, Crossbred
 from cryptographic_estimators.SDEstimator import SDEstimator, Prange, Dumer, BallCollision, BJMM, BJMMdw, BJMMpdw, BothMay, MayOzerov, Stern
 import pytest
 
@@ -17,6 +17,19 @@ def test_estimates_with_bjorklund():
         'Bjorklund': {'estimate': {'time': 95.50814347964285, 'memory': 36.48546485189783, 'parameters': {'lambda_': 16/49}}, 'additional_information': {}},
         'ExhaustiveSearch': {'estimate': {'time': 52.48921146923813, 'memory': 16.844129532345626, 'parameters': {}}, 'additional_information': {}}}
     assert expected_result == result
+
+
+def test_mq_raises_exception_with_bjorklund():
+    e = [
+        DinurFirst, DinurSecond, BooleanSolveFXL,
+        HybridF5, Lokshtanov, Crossbred,
+        F5, KPG, CGMTA, MHT
+    ]
+
+    with pytest.raises(TypeError, match="q must be equal to 2"):
+        Bjorklund(MQProblem(100, 50, 4))
+        # mq_estimator = MQEstimator(100, 50, 4, excluded_algorithms=e)
+        # mq_estimator.estimate()
 
 
 def test_sd_raises_error_when_invalid_parameters_are_passed():
