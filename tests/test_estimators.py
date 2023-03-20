e43087ce070c0755ad5665da714254a2bbd9c89d
ariane <arianezf@gmail.com>
2023-03-20 09:18:25 +0000

    run pytest

--- tests/test_estimators.py
+++ tests/test_estimators.py
@@ -28,8 +28,8 @@ def test_mq_raises_exception_with_bjorklund():
 
     with pytest.raises(TypeError, match="q must be equal to 2"):
         Bjorklund(MQProblem(100, 50, 4))
-        # mq_estimator = MQEstimator(100, 50, 4, excluded_algorithms=e)
-        # mq_estimator.estimate()
+        mq_estimator = MQEstimator(100, 50, 4, excluded_algorithms=e)
+        mq_estimator.estimate()
 
 
 def test_sd_raises_error_when_invalid_parameters_are_passed():
