3f921bbbb07c9fd6b9b38c4629beedd3ea4cdea7
Floyd Zweydinger <floyd.zweydinger@rub.de>
2023-03-29 14:41:05 +0200

    fixxed failing tests in test_sd

--- cryptographic_estimators/SDEstimator/__init__.py
+++ cryptographic_estimators/SDEstimator/__init__.py
@@ -2,4 +2,4 @@ from .sd_algorithm import SDAlgorithm
 from .sd_estimator import SDEstimator
 from .sd_helper import binom, min_max, _gaussian_elimination_complexity, _optimize_m4ri, _mem_matrix, _list_merge_complexity, _indyk_motwani_complexity, _mitm_nn_complexity, _list_merge_async_complexity
 from .sd_problem import SDProblem
-from .SDAlgorithms import BallCollision, BJMM, BJMMdw, BJMMpdw, BothMay, Dumer, MayOzerov, Prange, Stern
+from .SDAlgorithms import BallCollision, BJMM, BJMMdw, BJMMpdw, BJMM_plus, BothMay, Dumer, MayOzerov, Prange, Stern
