c3abca5426393e181f5975c2c505297adbea0ad2
fprod <franp.dev@protonmail.com>
2023-03-31 08:31:02 +0100

    Release v1.0.1 (#58)
    * fix: add missing estimator imports
    
    * fix(typo): update estimator name with the right casing
    
    * chore: update project version
    
    ---------
    
    Co-authored-by: Floyd Zweydinger <floyd.zweydinger@rub.de>
    Co-authored-by: AnonymousAccABC <andre.esser@tii.ae>
    Co-authored-by: javierverbel <javier.verbel@tii.ae>
    Co-authored-by: Memphisd <andre.esser@rub.de>
    Co-authored-by: javier.verbel <38933233+Javierverbel@users.noreply.github.com>
    Co-authored-by: FloydZ <11518727+FloydZ@users.noreply.github.com>
    Co-authored-by: ariane <arianezf@gmail.com>

--- cryptographic_estimators/__init__.py
+++ cryptographic_estimators/__init__.py
@@ -3,6 +3,9 @@ from .base_estimator import BaseEstimator
 from .base_problem import BaseProblem
 from .helper import ComplexityType, concat_pretty_tables, _truncate, round_or_truncate
 from . import SDEstimator
-from . import SDFqEstimator
 from . import MQEstimator
+from . import SDFqEstimator
+from . import PKEstimator
+from . import LEEstimator
+from . import PEEstimator
 from . import DummyEstimator
