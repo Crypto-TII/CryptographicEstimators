7ab38a918b6698beb03859402d96c673d076f4b2
ariane <ariane@leamind.es>
2023-04-03 15:55:25 +0100

    modify copyright template in script

--- scripts/create_copyright.py
+++ scripts/create_copyright.py
@@ -1,21 +1,21 @@
 import os
 
 COPYRIGHT = "# ****************************************************************************\n" + \
-"# Copyright 2023 Technology Innovation Institute\n" + \
-"# \n" + \
-"# This program is free software: you can redistribute it and/or modify\n" + \
-"# it under the terms of the GNU General Public License as published by\n" + \
-"# the Free Software Foundation, either version 3 of the License, or\n" + \
-"# (at your option) any later version.\n" + \
-"# \n" + \
-"# This program is distributed in the hope that it will be useful,\n" + \
-"# but WITHOUT ANY WARRANTY; without even the implied warranty of\n" + \
-"# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n" + \
-"# GNU General Public License for more details.\n" + \
-"# \n" + \
-"# You should have received a copy of the GNU General Public License\n" + \
-"# along with this program.  If not, see <https://www.gnu.org/licenses/>.\n" + \
-"# ****************************************************************************\n "
+    "# Copyright 2023 Technology Innovation Institute\n" + \
+    "#\n" + \
+    "# This program is free software: you can redistribute it and/or modify\n" + \
+    "# it under the terms of the GNU General Public License as published by\n" + \
+    "# the Free Software Foundation, either version 3 of the License, or\n" + \
+    "# (at your option) any later version.\n" + \
+    "#\n" + \
+    "# This program is distributed in the hope that it will be useful,\n" + \
+    "# but WITHOUT ANY WARRANTY; without even the implied warranty of\n" + \
+    "# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n" + \
+    "# GNU General Public License for more details.\n" + \
+    "#\n" + \
+    "# You should have received a copy of the GNU General Public License\n" + \
+    "# along with this program.  If not, see <https://www.gnu.org/licenses/>.\n" + \
+    "# ****************************************************************************"
 
 ROOT_FOLDER = "cryptographic_estimators"
 FILES_TO_READ = os.walk(ROOT_FOLDER)
@@ -28,6 +28,8 @@ for root, directories, files in FILES_TO_READ:
             with open(current_file_path, 'r+') as current_file:
                 file_content = current_file.read()
                 current_file.seek(0, 0)
+                # if COPYRIGHT in file_content:
+                #     print("copyright already in file", current_file_path)
                 if COPYRIGHT not in file_content:
                     current_file.write(f'{COPYRIGHT}\n\n{file_content}')
                     current_file.truncate()
