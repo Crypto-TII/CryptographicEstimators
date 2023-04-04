f7e1509e0c000b869a1ddaa73cc5faa9750c8a3d
Ariane Zanardi <38532645+arianezf@users.noreply.github.com>
2023-04-04 14:23:29 +0100

    Feat/create input dictionary script (#60)
    * feature: new input dictionary template
    
    * refactor: update append estimator dictionary input script
    
    ---------
    
    Co-authored-by: ariane <ariane@leamind.es>

--- scripts/append_estimator_to_input_dictionary.py
+++ scripts/append_estimator_to_input_dictionary.py
@@ -1,6 +1,7 @@
 """
 This module appends the structure for the new estimator into the input_dicitonary.json
 """
+import json
 
 
 class AppendEstimator():
@@ -10,63 +11,22 @@ class AppendEstimator():
         self.estimator_prefix = estimator_prefix
         self.estimator_label = input("Enter the estimator full name: ")
         self.upper_estimator_prefix = self.estimator_prefix.upper()
-        self.lower_estimator_prefix = self.estimator_prefix.lower()
 
     def write(self):
-        f = open("./input_dictionary.json", "rb+")
-        f.seek(-8, 2)
-        new_estimator_content = "},\n" + self.get_estimator_content()
-        f.write(new_estimator_content.encode('ascii'))
-        f.close()
+        with open("./input_dictionary.json", "r") as file:
+            data = json.load(file)
+
+            data["estimators"].append(
+                json.loads(self.get_estimator_content()))
+        json.dump(data, open("./input_dictionary.json", "w"), indent=2)
 
     def get_estimator_content(self):
-        tabs = "\t" * 2
-        template = f"{tabs}"+"{\n" + \
-            f"{tabs}" + f"\"estimator_id\": \"{self.upper_estimator_prefix}Estimator\",\n" + \
-            f"{tabs}" + f"\"algorithm_id\": \"{self.upper_estimator_prefix}Algorithm\",\n" + \
-            f"{tabs}" + f"\"display_label\": \"{self.estimator_label}\",\n" + \
-            f"{tabs}" + "\"landing_page_content\": \"# Dummy Markdown + Tex Landing Page\",\n" + \
-            self.get_problem_parameters() + \
-            self.get_estimators_parameters() + \
-            f"{tabs}" + "\"optional_parameters\":[]\n" + \
-            f"{tabs}" + "}\n" \
-            "\t]\n" \
-            "}"
-        return template
-
-    def get_problem_parameters(self):
-        tabs = "\t"
-        template = f"{tabs}" * 2 + "\"problem_parameters\": [\n" + \
-            f"{tabs}" * 3 + "{\n" + \
-            f"{tabs}" * 4 + "\"id\": \"Parameter1\",\n" + \
-            f"{tabs}" * 4 + "\"type\": \"number\",\n" + \
-            f"{tabs}" * 4 + "\"display_label\": \"Parameter 1\",\n" + \
-            f"{tabs}" * 4 + "\"placeholder\": \"Insert parameter\",\n" + \
-            f"{tabs}" * 4 + "\"tooltip\": \"This is the first problem parameter\"\n" + \
-            f"{tabs}" * 3 + "}\n" + \
-            f"{tabs}" * 2 + "],\n"
-        return template
-
-    def get_estimators_parameters(self):
-        tabs = "\t"
-        template = f"{tabs}" * 2 + "\"estimator_parameters\": [\n" + \
-            f"{tabs}" * 3 + "{\n" + \
-            f"{tabs}" * 4 + "\"id\": \"included_algorithms\",\n" + \
-            f"{tabs}" * 4 + "\"type\": \"multiple_selector\",\n" + \
-            f"{tabs}" * 4 + "\"direction\": \"column\",\n" + \
-            f"{tabs}" * 4 + "\"display_label\": \"Included algorithms\",\n" + \
-            f"{tabs}" * 4 + "\"tooltip\": \"Algorithms to include for optimization\",\n" + \
-            f"{tabs}" * 4 + "\"default_value\": [],\n" + \
-            f"{tabs}" * 4 + "\"excluded_algorithms\": [],\n" + \
-            f"{tabs}" * 4 + "\"options\": [],\n" + \
-            f"{tabs}" * 4 + "\"dependencies\": []\n" + \
-            f"{tabs}" * 3 + "}\n" + \
-            f"{tabs}" * 2 + "],\n"
-        return template
-
-    def get_algorithms_init_content(self):
-        template = f"from .sample import Sample\n" + \
-            "# TODO: Remember to add the algorithms to the import above"
+        template = open(
+            "./scripts/templates/input_dictionary_template.json").read()
+        template = template.replace("$$UPPER_CASE_PREFIX$$",
+                                    self.upper_estimator_prefix)
+        template = template.replace("$$DISPLAY_LABEL$$",
+                                    self.estimator_label)
         return template
 
 
