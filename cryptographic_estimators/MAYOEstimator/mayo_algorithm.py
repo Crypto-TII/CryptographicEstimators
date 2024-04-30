c596e5c6774e0625a7f28f7b2da5a281a8e6e0d0
fmerino21 <fmerino@unal.edu.co>
2024-04-30 01:42:38 -0500

    add: Documentation and test for the MAYO estimator

--- cryptographic_estimators/MAYOEstimator/mayo_algorithm.py
+++ cryptographic_estimators/MAYOEstimator/mayo_algorithm.py
@@ -16,8 +16,11 @@
 # ****************************************************************************
 
 
+from ..helper import ComplexityType
+from ..base_algorithm import BaseAlgorithm, optimal_parameter
 from ..base_algorithm import BaseAlgorithm
 from .mayo_problem import MAYOProblem
+from .mayo_helper import _optimize_k
 
 
 class MAYOAlgorithm(BaseAlgorithm):
@@ -28,8 +31,11 @@ class MAYOAlgorithm(BaseAlgorithm):
         INPUT:
 
         - ``problem`` -- MAYOProblem object including all necessary parameters
+        - ``w`` -- linear algebra constant (default: 2.81)
+        - ``h`` -- external hybridization parameter (default: 0)
         - ``memory_access`` -- specifies the memory access cost model (default: 0, choices: 0 - constant, 1 - logarithmic, 2 - square-root, 3 - cube-root or deploy custom function which takes as input the logarithm of the total memory usage)
         - ``complexity_type`` -- complexity type to consider (0: estimate, 1: tilde O complexity, default: 0)
+        - ``bit_complexities`` -- determines if complexity is given in bit operations or basic operations (default 1: in bit)
 
         """
         super(MAYOAlgorithm, self).__init__(problem, **kwargs)
@@ -44,14 +50,44 @@ class MAYOAlgorithm(BaseAlgorithm):
         if self._h < 0:
             raise ValueError("h must be >= 0")
         
+        n = self.problem.nvariables()
+        self.set_parameter_ranges('K', 0, n-1)
+        
+    @optimal_parameter
+    def K(self):
+        """
+        Return the optimal parameter `K` from Furue, Nakamura, and Takagi strategy
+
+        EXAMPLES::
+
+            sage: cryptographic_estimators.MAYOEstimator.mayo_algorithm import MAYOAlgorithm
+            sage: cryptographic_estimators.MAYOEstimator.mayo_problem import MAYOProblem
+            sage: E = MAYOAlgorithm(MAYOProblem(n=66, m=64, o=8, k=9, q=16))
+            sage: E.K()
+            16
+                
+        """
+        if self._optimal_parameters.get("K") is None:
+            n, m, _, k, q = self.problem.get_parameters()
+            if self.complexity_type == ComplexityType.ESTIMATE.value:
+                return _optimize_k(n, m, k, q)
+            elif self.complexity_type == ComplexityType.TILDEO.value:
+                return 0
+
+        return self._optimal_parameters.get("K")
+        
     def linear_algebra_constant(self):
         """
         Return the linear algebra constant
 
         TESTS::
 
+            sage: cryptographic_estimators.MAYOEstimator.mayo_algorithm import MAYOAlgorithm
+            sage: cryptographic_estimators.MAYOEstimator.mayo_problem import MAYOProblem
+            sage: E = MAYOAlgorithm(MAYOProblem(n=66, m=64, o=8, k=9, q=16))
+            sage: E.linear_algebra_constant()
+            2.81
 
-        
         """
         return self._w
 
---
+++ cryptographic_estimators/MAYOEstimator/mayo_helper.py
@@ -0,0 +1,51 @@
+# ****************************************************************************
+# Copyright 2023 Technology Innovation Institute
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ****************************************************************************
+
+from ..MQEstimator.MQAlgorithms import BooleanSolveFXL
+from ..MQEstimator import MQProblem
+from math import log2, inf
+from sage.functions.other import floor
+
+
+def _optimize_k(n: int, m: int, k: int, q: int):
+    """
+    Find the optimal parameter `K` from Furue, Nakamura, and Takagi strategy
+
+    INPUT:
+
+    - ``n`` -- number of variables
+    - ``m`` -- number of polynomials
+    - ``k`` -- whipping parameter
+    - ``q`` -- order of the finite field
+
+    """
+    (K, time) = (0, inf)
+
+    for i in range(0, n-1):
+        m_tilde = m - floor(((k*n)-i)/(m-i)) + 1
+        n_tilde = m_tilde - i
+
+        if n_tilde < 1: break
+
+        E = BooleanSolveFXL(MQProblem(n=n_tilde, m=m_tilde, q=q), bit_complexities=False)
+        t = i * log2(q) + E.time_complexity()
+
+        if t < time:
+            time = t
+            K = i
+
+    return K
