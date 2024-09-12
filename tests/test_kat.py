2bdd093f47fdd786550ef7b8e008c84b153fa7ec
Dioprz <55264980+Dioprz@users.noreply.github.com>
2024-09-12 01:11:52 -0500

    refactor: KAT testing logic polish
    - Improve functions and variable names.
    
    - Include types.
    
    - Update and create docstrings.

--- tests/test_kat.py
+++ tests/test_kat.py
@@ -1,45 +1,47 @@
 import inspect
 import importlib
+from itertools import starmap
+from typing import Tuple, Callable, Dict, Iterable, Any, List
 
 
-def extract_sc_path_with_info(input_dict: dict, path: tuple = ()) -> iter:
-    """
-    Recursively extracts test paths and their corresponding inputs/outputs tuples from the input dictionary.
+def extract_paths(
+    nested_dict: Dict[str, Any], path: Tuple[str, ...] = ()
+) -> Iterable[Tuple[Tuple[str, ...], List[Tuple[Any, Any]]]]:
+    """Recursively extracts internal estimators paths and their corresponding inputs/outputs tuples from the input dictionary.
 
-    This function traverses the input dictionary and yields tuples containing the test path
+    This function traverses the input dictionary and yields tuples containing the internal estimator path
     and its inputs/outputs tuples for each leaf node containing an "inputs_with_expected_outputs" key.
 
     Args:
-        input_dict: The input dictionary to traverse.
-        path: The current path in the dictionary (used for recursion).
+        nested_dict (Dict[str, Any]): The input dictionary to traverse.
+        path (Tuple[str, ...]): The current path in the dictionary (used for recursion).
 
     Yields:
-        Tuples containing the test path and its inputs/outputs tuples.
+        Tuple[Tuple[str, ...], List[Tuple[Any, Any]]]: Tuples containing the internal estimators and its inputs/outputs tuples.
     """
-    for key, value in input_dict.items():
+    for key, value in nested_dict.items():
         current_path = path + (key,)
 
         if isinstance(value, dict):
             if "inputs_with_expected_outputs" in value:
                 yield current_path, value["inputs_with_expected_outputs"]
             else:
-                yield from extract_sc_path_with_info(value, current_path)
+                yield from extract_paths(value, current_path)
 
 
-def import_sc_from_path(single_case_path: tuple):
-    """
-    Imports a single case function from a specified path.
+def import_internal_estimator(internal_estimator_path: Tuple[str, ...]) -> Callable:
+    """Imports a an internal estimator function from a specified path.
 
     Args:
-        single_case_path (tuple): A tuple representing the path to the single case function, relative
+        internal_estimator_path (Tuple[str, ...]): A tuple representing the path to the internal estimator function, relative
             to the caller's location. The last element of the tuple is the function name, and
             the preceding elements (if any) represent the module path.
 
     Returns:
-        function: The imported function.
+        Callable: The imported function.
 
     Example:
-        function = import_single_case_from_path(('sdfq', 'lee-brickell'))
+        function = import_internal_estimator(('sdfq', 'lee-brickell'))
     """
 
     caller_frame = inspect.currentframe().f_back
@@ -48,8 +50,8 @@ def import_sc_from_path(single_case_path: tuple):
     package_parts = caller_module.__name__.split(".")
     caller_path = ".".join(package_parts[:-1] + ["internal_estimators"])
 
-    single_case_module_path = ".".join(single_case_path[:-1])
-    function_name = single_case_path[-1]
+    single_case_module_path = ".".join(internal_estimator_path[:-1])
+    function_name = internal_estimator_path[-1]
 
     import_modpath = f"{caller_path}.{single_case_module_path}"
     module = importlib.import_module(import_modpath)
@@ -58,29 +60,34 @@ def import_sc_from_path(single_case_path: tuple):
     return function
 
 
-def kat_test(expected_output, actual_output, epsilon):
+def kat_test(expected_output: Any, actual_output: Any, epsilon: float):
+    """Asserts that the absolute difference between the expected and actual outputs is less than the specified epsilon.
+
+    The expected output comes from the KAT values produced from external estimators, and the actual ones are produced by our internals estimators.
+
+    Args:
+        expected_output (Any): The expected output value.
+        actual_output (Any): The actual output value.
+        epsilon (float): The maximum allowable difference between the expected and actual outputs.
+    """
     assert abs(expected_output - actual_output) < epsilon
 
 
-def test_all_estimators(yaml_references: dict):
-    """ """
+def test_all_estimators(kat: dict):
+    """Runs a KAT test for all internal estimators specified in the YAML references.
 
-    # for filename in yaml_references:
-    #     for internal_reference_name in yaml_references[filename]:
-    #         inputs_with_expected_outputs = yaml_references[filename][
-    #             internal_reference_name
-    #         ]["inputs_with_expected_outputs"]
-    #         for input, expected_output in inputs_with_expected_outputs:
-    #             internal_reference_function = import_sc_from_path(
-    #                 (filename, internal_reference_name)
-    #             )
-    #             actual_output, epsilon = internal_reference_function(input)
-    #             kat_test(expected_output, actual_output, epsilon)
+    This function extracts the internal estimator paths and their corresponding inputs/outputs from the
+    YAML references, imports the relevant internal estimator functions, and runs a standard KAT test
+    to verify the outputs against the expected values produced by external estimators.
 
-    sc_path_and_inputs_with_outputs = extract_sc_path_with_info(yaml_references)
+    Args:
+        yaml_references (Dict[str, Any]): A dictionary containing the KAT references.
+    """
 
-    for sc_path, inputs_with_outputs in sc_path_and_inputs_with_outputs:
+    int_estimator_paths_with_values = extract_paths(kat)
+
+    for internal_estimator_path, inputs_with_outputs in int_estimator_paths_with_values:
         inputs, expected_outputs = zip(*inputs_with_outputs)
-        single_case_function = import_sc_from_path(sc_path)
-        actual_outputs, epsilon = zip(*map(single_case_function, inputs))
-        assert map(kat_test, list(zip(expected_outputs, actual_outputs, epsilon)))
+        internal_estimator_function = import_internal_estimator(internal_estimator_path)
+        actual_outputs, epsilon = zip(*map(internal_estimator_function, inputs))
+        assert starmap(kat_test, list(zip(expected_outputs, actual_outputs, epsilon)))
