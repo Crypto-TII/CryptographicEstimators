9e3690d3b1da48fbeb5639da6bcc1914569bd5aa
Dioprz <55264980+Dioprz@users.noreply.github.com>
2024-09-28 02:02:52 -0500

    refactor: BIKE sage docstrings migration to python

--- cryptographic_estimators/BIKEEstimator/bike_problem.py
+++ cryptographic_estimators/BIKEEstimator/bike_problem.py
@@ -18,52 +18,42 @@ from .bike_constants import BIKE_DIMENSION, BIKE_SK_WEIGHT, BIKE_MSG_WEIGHT
 from ..base_problem import BaseProblem
 from math import log2
 
-
+#TODO: Fill with parameters
 class BIKEProblem(BaseProblem):
-    """
-    Construct an instance of BIKEProblem. Contains the parameters to optimize
-    over.
+    def __init__(self, r: int, w: int, t: int, **kwargs):
+        """Construct an instance of BIKEProblem.
 
-    INPUT:
+        Contains the parameters to optimize over.
 
-    - ``r`` -- code dimension
-    - ``w`` -- weight of secret key polynomial
-    - ``t`` -- weight of polynomials encoding messages
-    - ``memory_bound`` -- maximum allowed memory to use for solving the problem
-
-    """
-
-    def __init__(self, r: int, w: int, t: int, **kwargs):  # Fill with parameters
+        Args:
+            r (int): Code dimension
+            w (int): Weight of secret key polynomial
+            t (int): Weight of polynomials encoding messages
+            **kwargs: Additional keyword arguments
+                memory_bound: Maximum allowed memory to use for solving the problem
+        """
         super().__init__(**kwargs)
         self.parameters = {BIKE_DIMENSION: r, BIKE_SK_WEIGHT: w, BIKE_MSG_WEIGHT: t}
 
     def to_bitcomplexity_time(self, basic_operations: float):
-        """
-        Return the bit-complexity corresponding to a certain amount of basic_operations
-
-        INPUT:
-
-        - ``basic_operations`` -- Number of basic operations (logarithmic)
-
+        """Return the bit-complexity corresponding to a certain amount of basic_operations.
+    
+        Args:
+            basic_operations (float): Number of basic operations (logarithmic)
         """
         code_length = 2 * self.parameters[BIKE_DIMENSION]
         return basic_operations + log2(code_length)
 
     def to_bitcomplexity_memory(self, elements_to_store: float):
-        """
-        Return the memory bit-complexity associated to a given number of elements to store
-
-        INPUT:
-
-        - ``elements_to_store`` -- number of memory operations (logarithmic)
-
+        """Return the memory bit-complexity associated to a given number of elements to store.
+    
+        Args:
+            elements_to_store (float): Number of memory operations (logarithmic)
         """
         code_length = 2 * self.parameters[BIKE_DIMENSION]
         return elements_to_store + log2(code_length)
 
     def __repr__(self):
-        """
-        """
         r, w, t = self.get_parameters()
         rep = "BIKE instance with (r,w,t) = " + "(" + str(r) + "," + str(w) + "," + str(t) + ")"
         return rep
