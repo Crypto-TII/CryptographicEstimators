e5e379184a4c66927e36332dc08115b44475473b
Dioprz <55264980+Dioprz@users.noreply.github.com>
2024-09-28 01:25:13 -0500

    refactor: PK sage docstrings migration to python

--- cryptographic_estimators/PKEstimator/pk_estimator.py
+++ cryptographic_estimators/PKEstimator/pk_estimator.py
@@ -22,26 +22,24 @@ from math import inf
 
 
 class PKEstimator(BaseEstimator):
-    """
-    Construct an instance of Permuted Kernel Estimator
-
-    INPUT:
-
-    - ``n`` -- columns of the matrix
-    - ``m`` -- rows of the matrix
-    - ``q`` -- size of the field
-    - ``ell`` -- rows of the matrix whose permutation should lie in the kernel
-    - ``sd_parameters`` -- Dictionary of optional parameter arguments for SDEstimator used by SBC algorithm
-    - ``cost_for_list_operation`` -- Cost in Fq additions for one list operation in the SBC and KMP algorithm (default n-m)
-    - ``memory_for_list_element`` -- Memory in Fq elements for one list element in the SBC and KMP algorithm (default n-m)
-    - ``use_parity_row`` -- enables trick of appending extra (all one) row to the matrix, i.e., m -> m+1 (default:False)
-    - ``excluded_algorithms`` -- a list/tuple of excluded algorithms (default: None)
-    - ``nsolutions`` -- no. of solutions
-
-    """
     excluded_algorithms_by_default = []
-
     def __init__(self, n: int, m: int, q: int, ell: int = 1, memory_bound=inf, **kwargs):
+        """Construct an instance of Permuted Kernel Estimator.
+
+        Args:
+            n (int): Columns of the matrix.
+            m (int): Rows of the matrix.
+            q (int): Size of the field.
+            ell (int): Rows of the matrix whose permutation should lie in the kernel.
+            memory_bound: Memory bound.
+            **kwargs: Additional keyword arguments.
+                sd_parameters (dict): Optional parameter arguments for SDEstimator used by SBC algorithm.
+                cost_for_list_operation: Cost in Fq additions for one list operation in the SBC and KMP algorithm (default n-m).
+                memory_for_list_element: Memory in Fq elements for one list element in the SBC and KMP algorithm (default n-m).
+                use_parity_row (bool): Enables trick of appending extra (all one) row to the matrix, i.e., m -> m+1 (default: False).
+                excluded_algorithms (list or tuple): A list/tuple of excluded algorithms (default: None).
+                nsolutions: No. of solutions.
+        """
         if not kwargs.get("excluded_algorithms"):
             kwargs["excluded_algorithms"] = []
 
@@ -51,22 +49,19 @@ class PKEstimator(BaseEstimator):
 
     def table(self, show_quantum_complexity=0, show_tilde_o_time=0,
               show_all_parameters=0, precision=1, truncate=0):
-        """
-        Print table describing the complexity of each algorithm and its optimal parameters
+        """Print table describing the complexity of each algorithm and its optimal parameters.
+    
+        Args:
+            show_quantum_complexity (int): Show quantum time complexity (default: 0)
+            show_tilde_o_time (int): Show Ō time complexity (default: 0)
+            show_all_parameters (int): Show all optimization parameters (default: 0)
+            precision (int): Number of decimal digits output (default: 1)
+            truncate (int): Truncate rather than round the output (default: 0)
 
-        INPUT:
-
-        - ``show_quantum_complexity`` -- show quantum time complexity (default: true)
-        - ``show_tilde_o_time`` -- show Ō time complexity (default: true)
-        - ``show_all_parameters`` -- show all optimization parameters (default: true)
-        - ``precision`` -- number of decimal digits output (default: 1)
-        - ``truncate`` -- truncate rather than round the output (default: false)
-
-        EXAMPLES::
-
-            sage: from cryptographic_estimators.PKEstimator import PKEstimator
-            sage: A = PKEstimator(n=40,m=10,q=7,ell=2)
-            sage: A.table()
+        Examples:
+            >>> from cryptographic_estimators.PKEstimator import PKEstimator
+            >>> A = PKEstimator(n=40,m=10,q=7,ell=2)
+            >>> A.table()
             +-----------+----------------+
             |           |    estimate    |
             +-----------+-------+--------+
@@ -76,11 +71,12 @@ class PKEstimator(BaseEstimator):
             | SBC       | 137.6 |   42.8 |
             +-----------+-------+--------+
 
-        TESTS::
-
-            sage: from cryptographic_estimators.PKEstimator import PKEstimator
-            sage: A = PKEstimator(n=100,m=50,q=31,ell=2)
-            sage: A.table(precision=3, show_all_parameters=1) # long time
+        Tests:
+            >>> if skip_long_doctests:
+            ...     pytest.skip()
+            >>> from cryptographic_estimators.PKEstimator import PKEstimator
+            >>> A = PKEstimator(n=100,m=50,q=31,ell=2)
+            >>> A.table(precision=3, show_all_parameters=1) # long time
             +-----------+------------------------------------------------+
             |           |                    estimate                    |
             +-----------+---------+---------+----------------------------+
@@ -89,7 +85,6 @@ class PKEstimator(BaseEstimator):
             | KMP       | 243.808 | 243.722 |         {'u': 24}          |
             | SBC       | 241.319 | 236.722 | {'d': 1, 'w': 38, 'w1': 2} |
             +-----------+---------+---------+----------------------------+
-
         """
         super(PKEstimator, self).table(show_quantum_complexity=show_quantum_complexity,
                                        show_tilde_o_time=show_tilde_o_time,
