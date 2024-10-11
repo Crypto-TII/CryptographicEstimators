fbe0f329701162b7b84df82dc727eaaaac6a1b52
Floyd <floyd.zweydinge+gitr@rub.de>
2024-10-11 12:04:59 +0200

    fixed a few typos

--- tests/internal_estimators/le.py
+++ tests/internal_estimators/le.py
@@ -39,7 +39,7 @@ def beullens_range(input: tuple, epsilon: float = 1.2):
 def bbps_1(input: tuple, epsilon: float = 0.1):
     n, k, q = input
 
-    actual_complexity = BBPS(LEProblem(n, k, q), **BEULLENS_PARAMS).time_complexity()
+    actual_complexity = BBPS(LEProblem(n, k, q), **BBPS_PARAMS).time_complexity()
 
     return actual_complexity, epsilon
 
@@ -48,7 +48,7 @@ def bbps_2(input: tuple, epsilon: float = 1):
     """For small q we need to allow for a slightly larger tolerance, because the coupon collector approximation is less accurate."""
     n, k, q = input
 
-    actual_complexity = BBPS(LEProblem(n, k, q), **BEULLENS_PARAMS).time_complexity()
+    actual_complexity = BBPS(LEProblem(n, k, q), **BBPS_PARAMS).time_complexity()
 
     return actual_complexity, epsilon
 
@@ -56,8 +56,8 @@ def bbps_2(input: tuple, epsilon: float = 1):
 def bbps_range(input: tuple, epsilon: float = 1):
     n, k, q = input
 
-    candidate_1 = Beullens(LEProblem(n, k, q), **BEULLENS_PARAMS).time_complexity()
-    candidate_2 = Beullens(LEProblem(n, k, q), **BEULLENS_PARAMS).time_complexity()
+    candidate_1 = BBPS(LEProblem(n, k, q), **BBPS_PARAMS).time_complexity()
+    candidate_2 = BBPS(LEProblem(n, k, q), **BBPS_PARAMS).time_complexity()
     actual_complexity = min(candidate_1, candidate_2)
 
     return actual_complexity, epsilon
