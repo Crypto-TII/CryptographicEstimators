7a06c4fa161df1add13b31c3209b8f9d741382ee
PingFloyd <zweydfg8+github@rub.de>
2025-01-30 17:10:25 +0100

    updated shell.nix and removed sage

--- shell.nix
+++ shell.nix
@@ -1,21 +1,6 @@
 with import <nixpkgs> {};
 let
-  
-  # TODO combine those two
-  mysage = sage.override { 
-   	extraPythonPackages = ps: with ps; [ 
-	 	prettytable 
-		scipy
-		sphinx
-		furo
-		autopep8
-		pip
-		pytest
-	]; 
-	requireSageTests = false;
-  };
-
-  my-python = pkgs.python3;
+ my-python = pkgs.python3;
   mypython = my-python.withPackages (p: with p; [
 	prettytable
 	scipy
@@ -35,9 +20,7 @@ stdenv.mkDerivation {
 
   buildInputs = [ 
     mypython
-	mysage
 	ripgrep
-    nodePackages.pyright
 	tree
   ];
 
