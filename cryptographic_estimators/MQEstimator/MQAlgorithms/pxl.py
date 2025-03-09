4b9038e2f861eea288d7bae5579f1e2ec0387487
fmerino21 <fmerino@unal.edu.co>
2025-03-08 21:21:29 -0500

    add: basic time_complexity

---
+++ cryptographic_estimators/MQEstimator/MQAlgorithms/pxl.py
@@ -0,0 +1,84 @@
+# ****************************************************************************
+# Copyright 2023 Technology Innovation Institute
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ****************************************************************************
+
+
+from cryptographic_estimators.MQEstimator.mq_algorithm import MQAlgorithm
+from cryptographic_estimators.MQEstimator.mq_problem import MQProblem
+from cryptographic_estimators.MQEstimator.series.hilbert import HilbertSeries
+from cryptographic_estimators.base_algorithm import optimal_parameter
+from math import log2, binomial
+
+
+class PXL(MQAlgorithm):
+    def __init__(self, problem: MQProblem, **kwargs):
+        """Construct an instance of DummyAlgorithm1 estimator.
+
+        ---------------------
+
+        Args:
+            
+
+        Examples:
+
+        """
+        super().__init__(problem, **kwargs)
+        self._name = "PXL"
+
+        n, m, q = problem.get_problem_parameters()
+
+        self.set_parameter_ranges("k", 1, n//2)
+
+
+    @optimal_parameter
+    def k(self):
+        """Return the optimal k
+
+        Examples:
+            
+        """
+        return self._get_optimal_parameter("k")
+
+    def _compute_time_complexity(self, parameters: dict):
+        """Return the time complexity of the algorithm for a given set of parameters.
+    
+        Args:
+            parameters (dict): Dictionary including the parameters.
+        """
+        n, m, q = self.problem.get_problem_parameters()
+        w = self.linear_algebra_constant()
+        k = parameters["k"]
+        H = HilbertSeries(n=n - k, degrees=[2] * m)
+        D = H.first_nonpositive_coefficient_up_to_degree()
+        alpha = sum(
+            [
+                H.coefficient_of_degree(i)
+                for i in range(D + 1)
+            ]
+        )
+        time_1 = 2 * log2(k) + log2(alpha) + log2(binomial(n - k +D, k) * binomial(n + D, D))
+        time_2 = k * log2(q) + log2(binomial(k + D, D))
+        time_3 = k * log2(q) + w * log2(alpha)
+
+        return max(time_1, time_2, time_3)
+
+    def _compute_memory_complexity(self, parameters: dict):
+        """Return the memory complexity of the algorithm for a given set of parameters.
+    
+        Args:
+            parameters (dict): Dictionary including the parameters.
+        """
+        return 0
