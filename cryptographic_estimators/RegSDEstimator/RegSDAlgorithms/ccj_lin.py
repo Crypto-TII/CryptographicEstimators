05eaad85dc8e90e89c7937b0db3ab9af76f95053
Floyd <zweydfg8+github@rub.de>
2025-05-22 08:23:57 +0200

    removed double licenses

--- cryptographic_estimators/RegSDEstimator/RegSDAlgorithms/ccj_lin.py
+++ cryptographic_estimators/RegSDEstimator/RegSDAlgorithms/ccj_lin.py
@@ -17,23 +17,6 @@
 # under the License.
 # ****************************************************************************
 
-# ****************************************************************************
-# Copyright 2023 Technology Innovation Institute
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-# ****************************************************************************
-
 from ..regsd_algorithm import RegSDAlgorithm
 from ..regsd_problem import RegSDProblem
 from math import log2
